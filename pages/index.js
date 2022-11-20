292cad936a193570eae4b6cc3fe7617ed0e6df37
FeBalla <fballadares@uc.cl>
2022-11-20 01:35:07 -0300

    basic setup

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Head from "next/head"
+
+export default function Home() {
+  return (
+    <div className="bg-gray-50">
+      <Head>
+        <title>Bitácoras - Liderazgo, Juegos y Recreación</title>
+        <meta name="description" content="Bitácoras - Liderazgo, Juegos y Recreación" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>Hola!</main>
+    </div>
+  )
+}
