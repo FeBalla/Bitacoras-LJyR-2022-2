3bfce5fe3cc87cb7c827bc69c7373a21e4013377
FeBalla <fballadares@uc.cl>
2022-11-20 22:05:15 -0300

    adds mock pagination and games data

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,34 @@
 import Head from "next/head"
-import { useRouter } from "next/router"
+import { useState, useEffect } from "react"
 import GameCard from "../components/GameCard"
-import { gamesData } from "../data/games"
+import gamesData from "../data/gamesData.json"
 
 export default function Home() {
-  const router = useRouter()
-  const { page = 1 } = router.query
+  const [page, setPage] = useState(1)
+  const [startGame, setStartGame] = useState(0)
+  const [endGame, setEndGame] = useState(6)
+  const maxPage = Math.floor(gamesData.length / 6)
+  console.log(gamesData.length)
+
+  useEffect(() => {
+    const newStartGame = Math.min(6 * page, gamesData.length - 1)
+    const newEndGame = Math.min(newStartGame + 6, gamesData.length - 1)
+    setStartGame(newStartGame)
+    setEndGame(newEndGame)
+  }, [page])
+
+  const decreasePage = () => {
+    if (page > 0) {
+      setPage(page - 1)
+    }
+  }
+
+  const increasePage = () => {
+    if (page < maxPage) {
+      setPage(page + 1)
+    }
+  }
+
   return (
     <>
       <Head>
@@ -19,27 +42,30 @@ export default function Home() {
           <h1 className="text-2xl lg:text-3xl font-semibold text-gray-900">
             Bitácoras - Liderazgo, Juegos y Recreación
           </h1>
-          <h3 className="italic">(Página {page})</h3>
+          <h3 className="italic">(Página {page} de {maxPage})</h3>
         </div>
 
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
-          {gamesData.map((game) => {
+          {gamesData.slice(startGame, endGame).map((game) => {
             return <GameCard key={game.id} data={game} />
           })}
         </div>
 
         <div className="flex gap-7 md:gap-10 my-5 lg:my-8">
           <button
-            className="rounded-md bg-cyan-700 hover:bg-cyan-800 text-white text-sm 
+            className="rounded-md bg-blue-700 hover:bg-blue-800 text-white text-sm 
             md:text-base px-3 md:px-5 py-2 md:py-3 disabled:opacity-40 disabled:pointer-events-none"
             disabled={page <= 1}
+            onClick={decreasePage}
           >
             Página anterior
           </button>
 
           <button
-            className="rounded-md bg-cyan-700 hover:bg-cyan-800 text-white text-sm 
+            className="rounded-md bg-blue-700 hover:bg-blue-800 text-white text-sm 
             md:text-base px-3 md:px-5 py-2 md:py-3 disabled:opacity-40 disabled:pointer-events-none"
+            disabled={page >= maxPage}
+            onClick={increasePage}
           >
             Página siguiente
           </button>
