b9bfeef66bd63f16070dd56f441a10bfefbd1a17
FeBalla <fballadares@uc.cl>
2022-12-29 02:37:16 -0300

    fix: link in navbar

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -6,15 +6,15 @@ const NavBar = () => {
       className="flex w-full sticky top-0 z-50 justify-center md:justify-end shadow-md px-5 py-3 
       md:px-16 md:py-5 bg-white font-semibold"
     >
-      <div className="flex gap-5">
+      <nav className="flex gap-5">
         <Link href="/" className="hover:text-gray-700">
           Bitácoras
         </Link>
 
-        <Link href="/reflection" className="hover:text-gray-700">
+        <Link href="/reflexiones" className="hover:text-gray-700">
           Reflexión
         </Link>
-      </div>
+      </nav>
     </header>
   )
 }
