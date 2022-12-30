2ba3427ee20dc07a5bec90da11b370ebf6ed2232
FeBalla <fballadares@uc.cl>
2022-12-30 01:00:06 -0300

    fix: footer at bottom

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="flex flex-col text-center text-xs md:text-sm gap-1 my-5">
+    <footer className="flex flex-col text-center text-xs md:text-sm gap-1 py-5 bg-gray-100">
       <h4>Hecho con 💜 para el curso Liderazgo, Juegos y Recreación - Sección 3 (2022-2)</h4>
       <h5>Fernando Balladares - Martín Orrego - Sebastián Breguel</h5>
     </footer>
---
+++ components/layout/Layout.tsx
@@ -0,0 +1,20 @@
+import Footer from "../Footer"
+import NavBar from "../NavBar"
+
+type LayoutProps = {
+  children: JSX.Element
+}
+
+const Layout = ({ children }: LayoutProps) => {
+  return (
+    <div className="flex flex-col h-screen justify-between">
+      <NavBar />
+      <main className="mb-auto">
+        {children}
+      </main>
+      <Footer />
+    </div>
+  )
+}
+
+export default Layout
