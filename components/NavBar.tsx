bf46bb8e8f047c0525cb7584fa64ead1dbd0858b
FeBalla <fballadares@uc.cl>
2022-12-29 23:03:39 -0300

    change active tab color

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,17 +1,25 @@
 import Link from "next/link"
+import { useRouter } from "next/router"
 
 const NavBar = () => {
+  const router = useRouter()
   return (
     <header
       className="flex w-full sticky top-0 z-50 justify-center md:justify-end shadow-md px-5 py-3 
-      md:px-16 md:py-5 bg-white font-semibold"
+      md:px-16 md:py-5 bg-white"
     >
       <nav className="flex gap-5">
-        <Link href="/" className="hover:text-gray-700">
+        <Link
+          href="/"
+          className={router.pathname === "/" ? "font-semibold" : ""}
+        >
           Bitácoras
         </Link>
 
-        <Link href="/reflexiones" className="hover:text-gray-700">
+        <Link
+          href="/reflexiones"
+          className={router.pathname === "/reflexiones" ? "font-semibold" : ""}
+        >
           Reflexión
         </Link>
       </nav>
