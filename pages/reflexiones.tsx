bece2f4a61c4bc376eb83eabfbc82818bff98b8b
FeBalla <fballadares@uc.cl>
2022-12-30 01:29:52 -0300

    fix: grid cols in responsivness

--- pages/reflexiones.tsx
+++ pages/reflexiones.tsx
@@ -36,7 +36,7 @@ const Home = () => {
           <h1 className="font-semibold text-2xl md:text-3xl">Reflexiones</h1>
 
           {data && (
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+            <div className="grid grid-cols-1 lg:grid-cols-3 gap-5">
               {data.reflections.map((reflection) => {
                 return <ReflectionCard key={reflection.id} reflection={reflection} />
               })}
