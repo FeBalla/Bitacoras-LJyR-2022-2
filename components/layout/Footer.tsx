625d4f94977d2516d14630b8d6d6a1a78e1e6117
FeBalla <fballadares@uc.cl>
2022-12-30 01:14:49 -0300

    add: dev author in footer

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,8 +1,19 @@
 const Footer = () => {
   return (
-    <footer className="flex flex-col text-center text-xs md:text-sm gap-1 py-5 bg-gray-100">
-      <h4>Hecho con 💜 para el curso Liderazgo, Juegos y Recreación - Sección 3 (2022-2)</h4>
+    <footer className="flex flex-col text-center text-xs md:text-sm gap-1 py-5 bg-gray-50">
+      <h4>Hecho con 💜 para el curso Liderazgo, Juegos y Recreación del 2022-2</h4>
       <h5>Fernando Balladares - Martín Orrego - Sebastián Breguel</h5>
+      <div>
+        {"Desarrollado por "}
+        <a
+          href="https://github.com/FeBalla"
+          target="_blank"
+          rel="noreferrer"
+          className="font-semibold text-blue-800 hover:text-blue-900"
+        >
+          @FeBalla
+        </a>
+      </div>
     </footer>
   )
 }
