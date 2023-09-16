8687516c43bb99a039cb0cbd012da65bc5f92d79
FeBalla <fballadares@uc.cl>
2023-09-16 02:39:09 -0300

    restyle gamecard text

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -8,22 +8,23 @@ type GameCardProps = {
 const GameCard = ({ game }: GameCardProps) => {
   return (
     <div className="flex flex-col rounded-md gap-2 border p-3">
-      <div className="w-full h-52 relative">
+
+      <div className="text-left">
+        <h2 className="font-semibold text-2xl text-gray-800">{game.name}</h2>
+      </div>
+
+      <div className="w-full h-52 relative my-2">
         <ImageWithFallback src={game.imgUrl} alt={`Imagen del juego ${game.name}`} />
       </div>
 
-      <div className="text-center">
-        <h2 className="font-semibold text-xl">{game.name}</h2>
+      <div>
+        <h3 className="font-semibold text-lg mb-2 text-gray-700">Descripción</h3>
+        <p className="text-sm md:text-base text-gray-600 text-justify">{game.description}</p>
       </div>
 
       <div>
-        <h3 className="font-semibold text-lg mb-0.5">Descripción</h3>
-        <p className="text-sm md:text-base">{game.description}</p>
-      </div>
-
-      <div>
-        <h3 className="font-semibold text-lg mb-0.5">Objetivos</h3>
-        <p className="text-sm md:text-base">{game.objectives}</p>
+        <h3 className="font-semibold text-lg mb-2 text-gray-700">Objetivos</h3>
+        <p className="text-sm md:text-base text-gray-600 text-justify">{game.objectives}</p>
       </div>
     </div>
   )
