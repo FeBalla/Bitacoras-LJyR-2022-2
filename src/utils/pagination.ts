a6abf69676e135e9bee3f48b56c069daa2b80181
FeBalla <fballadares@uc.cl>
2023-09-16 01:22:31 -0300

    refactor: docstring for util functions and refactor ts-types

--- src/utils/pagination.ts
+++ src/utils/pagination.ts
@@ -1,16 +1,35 @@
-import { GamesQuery } from "../graphql/generated"
+import type { PageInfo, Aggregate } from "../graphql/generated"
 
-type GameConnection = GamesQuery["gamesConnection"]
+type ItemConnection = {
+  pageInfo: PageInfo
+  aggregate: Aggregate
+}
 
+type PaginationConfig = {
+  currentPage: number
+  itemFrom: number
+  itemTo: number
+  itemsPerPage: number
+  totalItems: number
+  totalPages: number
+  isPreviousDisabled: boolean
+  isNextDisabled: boolean
+  pagesToShow: number[]
+}
 
+/**
+ * Obtains the pages that should be displayed in the pagination, considering the
+ * current page, the total pages and taking into account that the current page
+ * should be relatively centered between the previous and following pages.
+ * @param currentPage The current page number.
+ * @param totalPages The total number of pages.
+ * @returns An array with the pages that should be displayed in order.
+ */
 const _getPagesToShow = (currentPage: number, totalPages: number): number[] => {
   // How many buttons in the pagination will be displayed.
   const MAX_PAGE_BUTTONS_DISPLAYED = 8
-  const lastButtonPage = Math.min(
-    totalPages,
-    Math.max(MAX_PAGE_BUTTONS_DISPLAYED, currentPage + 2)
-  )
-
+  // The last button must not pass the upper limit.
+  const lastButtonPage = Math.min(totalPages, Math.max(MAX_PAGE_BUTTONS_DISPLAYED, currentPage + 2))
   let page = Math.max(1, lastButtonPage - (MAX_PAGE_BUTTONS_DISPLAYED - 1))
   const pagesToShow = []
 
@@ -21,37 +40,33 @@ const _getPagesToShow = (currentPage: number, totalPages: number): number[] => {
   return pagesToShow
 }
 
-type UsePaginationState = {
-  itemFrom: number
-  itemTo: number
-  totalItems: number
-  isPreviousDisabled: boolean
-  isNextDisabled: boolean
-  currentPage: number
-  itemsPerPage: number
-  pagesToShow: number[]
-  totalPages: number
-}
-
+/**
+ * Gets a complete configuration for the pagination component based on the
+ * current page and the results of the query.
+ * @param currentPage An object with the current page configuration.
+ * @param gameConnection An object with the page meta data based on the
+ * query results (i.e. the total number of items).
+ * @returns An object with the complete configuration for the component.
+ */
 const getPaginationConfig = (
   currentPage: any,
-  pageMetaData: GameConnection,
-): UsePaginationState => {
+  gameConnection: ItemConnection
+): PaginationConfig => {
   const gamesPerPage = currentPage.itemsPerPage || 6
-  const totalItems = pageMetaData.aggregate.count
+  const totalItems = gameConnection.aggregate.count
   const totalPages = Math.ceil(totalItems / gamesPerPage)
   const pagesToShow = _getPagesToShow(currentPage.page, totalPages)
 
   return {
+    currentPage: currentPage.page,
     itemFrom: currentPage.itemsToSkip,
     itemTo: Math.min(currentPage.itemsToSkip + gamesPerPage, totalItems),
     itemsPerPage: gamesPerPage,
     totalItems: totalItems,
+    totalPages: totalPages,
     isPreviousDisabled: currentPage.page === 1,
     isNextDisabled: currentPage.page === totalPages,
-    currentPage: currentPage.page,
     pagesToShow: pagesToShow,
-    totalPages: totalPages,
   }
 }
 
