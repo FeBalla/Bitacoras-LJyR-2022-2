fa767160d010408b7b4812a99511bcf3261980b5
FeBalla <fernando.balladares.03@gmail.com>
2025-07-06 23:42:40 -0400

    Fix: Page metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,10 @@
 import { ApolloClientProvider } from "~/components/ApolloClientProvider"
-import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
 import { Layout } from "~/components/ui-blocks/Layout"
 import React from "react"
 
 const inter = Inter({ subsets: ["latin"] })
-
-export const metadata: Metadata = {
-  title: "Lista de Juegos",
-  description: "Aplicación para explorar juegos",
-}
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,13 @@
 import { LoadingSpinner } from "~/components/ui-blocks/LoadingSpinner"
 import { Suspense } from "react"
 import { GamesListPageContent } from "~/components/page-content/GamesListPageContent"
+import type { Metadata } from "next"
+
+export const metadata: Metadata = {
+  title: "ludolab | Juegos",
+  description: `Descubre una amplia variedad de juegos y dinámicas de grupo para
+                disfrutar con amigos y compañeros.`,
+}
 
 export default function GamesListPage() {
   return (
